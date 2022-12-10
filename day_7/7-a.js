fe12f246864d205ba45681eda4450dadcab15e2b
Colleen Handel <handelc22@gmail.com>
2022-12-10 15:35:07 -0600

    Day 7

---
+++ day_7/7-a.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const data = fs.readFileSync('./7-data.txt',
+            {encoding:'utf8', flag:'r'});
+
+var rows = data.split("\n");
+
+var directories = {};
+
+var current_path = [];
+
+for (var i = 0; i < rows.length; i++) {
+  var row = rows[i];
+
+  if (row.slice(0, 4) === '$ cd') {
+    var direction = row.slice(5);
+    if (direction === '..') {
+      current_path.pop();
+    } else {
+      if (direction === '/') {
+        current_path = ['/'];
+      } else {
+        current_path.push(direction);
+      }
+      var current_directory_path = current_path.join('-');
+      if (!directories[current_directory_path]) {
+        directories[current_directory_path] = 0;
+      }
+    }
+  } else if (row.slice(0, 4) === '$ ls') {
+    var output_row_num = 1;
+    var output_row = rows[i + output_row_num];
+    while (output_row && output_row.slice(0, 1) !== '$') {
+      if (output_row.slice(0, 3) !== 'dir') {
+        var size = Number(output_row.split(' ')[0]);
+        for (var j = 1; j <= current_path.length; j++) {
+          var current_directory_path = current_path.slice(0, j).join('-');
+          directories[current_directory_path] += size;
+        }
+      }
+      output_row_num++;
+      output_row = rows[i + output_row_num];
+    }
+    i += output_row_num - 1;
+  }
+}
+
+var total = 0;
+var directory_list = Object.keys(directories);
+for (var k = 0; k < directory_list.length; k++) {
+  if (directories[directory_list[k]] <= 100000) {
+    total += directories[directory_list[k]];
+  }
+}
+
+console.log(total);
